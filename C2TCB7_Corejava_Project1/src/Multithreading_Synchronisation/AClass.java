f401970dc065c3eaea9a89a260337f7a78a20309
VEENA199-VEE <96383117+VEENA199-VEE@users.noreply.github.com>
2022-01-31 20:00:04 +0530

    First Commit

---
+++ C2TCB7_Corejava_Project1/src/Multithreading_Synchronisation/AClass.java
@@ -0,0 +1,32 @@
+package Multithreading_Synchronisation;
+
+public class AClass {
+	int bal;
+	
+	synchronized void deposit(int amt)
+	{
+		System.out.println("Getting balance...");
+		int oldbal=getBalance();
+		bal=oldbal+amt;
+		setBalance(bal);
+	}
+	int getBalance()
+	{
+		return Account.Balance;
+	}
+	void setBalance(int amt)
+	{
+		try
+		{
+			Thread.sleep(3000);
+		}
+		
+		catch(Exception e)
+		{
+		}
+		Account.Balance=amt;
+		}
+}
+	
+
+
---
+++ C2TCB7_Corejava_Project1/src/Multithreading_Synchronisation/Account.java
@@ -0,0 +1,30 @@
+package Multithreading_Synchronisation;
+
+public class Account {
+	static int Balance=10000;
+
+	public static void main(String[] args) {
+		AClass oa=new AClass();
+		
+		DepositThread dt1=new DepositThread(oa,5000,"First");
+		DepositThread dt2=new DepositThread(oa,4000,"Second");
+		
+		dt1.start();
+		dt2.start();
+		try
+		{
+			dt1.join();
+			dt2.join();
+		}
+		catch(Exception e)
+		{
+		}
+		System.out.println("Total balance is: "+Balance);
+		}
+		
+		
+		
+		
+	}
+
+
