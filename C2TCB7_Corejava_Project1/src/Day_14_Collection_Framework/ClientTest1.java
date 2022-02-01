bc0900f79718bc1af9a2088ee7a045b9262dedfa
VEENA199-VEE <96383117+VEENA199-VEE@users.noreply.github.com>
2022-02-01 09:32:14 +0530

    first Commit

---
+++ C2TCB7_Corejava_Project1/src/Day_14_Collection_Framework/ClientTest1.java
@@ -0,0 +1,22 @@
+package Day_14_Collection_Framework;
+
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+public class ClientTest1 {
+
+    public static void main(String[] args) {
+        Buffer buffer = new Buffer();
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        // Calling one producer
+        executorService.execute(new ProducerTask(buffer));
+        // Calling three consumers
+        executorService.execute(new ConsumerTask(buffer));
+        executorService.execute(new ConsumerTask(buffer));
+        executorService.execute(new ConsumerTask(buffer));
+        executorService.shutdown();
+    }
+}
+
